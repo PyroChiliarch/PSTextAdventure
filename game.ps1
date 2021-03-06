3934f9d3dc92bec100b8b0ec3f54852478a994c0
PyroChiliarch <pyrochiliarch@gmail.com>
2021-03-06 20:17:38 +1100

    Input Added
    Input Added
    Movement and Console

--- game.ps1
+++ game.ps1
@@ -1,7 +1,6 @@
 #Objects
 
-class BufferCell
-{
+class BufferCell {
     #Properties
     [int]$depth
     [char]$char
@@ -22,29 +21,11 @@ class BufferCell
     }
 }
 
-class Sprite
-{
-    [char[,]]$image
-    [int]$width
-    [int]$height
-    [int]$xOrigin
-    [int]$yOrigin
-
-
-    Sprite ([int]$width, [int]$height, [int]$xOrigin, [int]$yOrigin, [char[,]]$image) {
-        $this.width = $width
-        $this.height = $height
-        $this.xOrigin = $xOrigin
-        $this.yOrigin = $yOrigin
-        $this.image = New-Object 'char[,]' $this.width,$this.height
-    }
-}
 
 
 
 
-class ViewPort
-{
+class ViewPort {
     #Contains 2 frame buffers which can be written to drawn
     
     #Properties
@@ -52,35 +33,34 @@ class ViewPort
     [int]$height
 
     #Frame buffer that contains changes
-    [BufferCell[,]]$frameBuffer
+    [BufferCell[, ]]$frameBuffer
     [BufferCell]$defaultCell
 
     #Screen Buffer contains the last drawn screen
-    [BufferCell[,]]$screenBuffer
+    [BufferCell[, ]]$screenBuffer
 
     #Constructor
-    ViewPort ([int]$width, [int]$height, [BufferCell]$defaultCell)
-    {
+    ViewPort ([int]$width, [int]$height, [BufferCell]$defaultCell) {
         $this.width = $width
         $this.height = $height
         $this.defaultCell = $defaultCell
 
         #Create frameBuffer as 2 dimensional array
         #Fill with empty defaultCells
-        $this.frameBuffer = New-Object 'BufferCell[,]' $width,$height
+        $this.frameBuffer = New-Object 'BufferCell[,]' $width, $height
         for ($x = 0; $x -lt $width; $x++) {
             for ($y = 0; $y -lt $height; $y++) {
-                $this.frameBuffer[$x ,$y] = [BufferCell]::new($defaultCell.depth, $defaultCell.char)
+                $this.frameBuffer[$x , $y] = [BufferCell]::new($defaultCell.depth, $defaultCell.char)
             }
         }
     }
 
 
     #Methods
-    [void] ClearFrameBuffer ([viewPort]$self) {
-        for ($x = 0; $x -lt $self.width; $x++) {
-            for ($y = 0; $y -lt $self.height; $y++) {
-                $this.frameBuffer[$x ,$y] = [BufferCell]::new($this.defaultCell.depth, $this.defaultCell.char)
+    [void] ClearFrameBuffer () {
+        for ($x = 0; $x -lt $this.width; $x++) {
+            for ($y = 0; $y -lt $this.height; $y++) {
+                $this.frameBuffer[$x , $y] = [BufferCell]::new($this.defaultCell.depth, $this.defaultCell.char)
                 
             }
         }
@@ -101,7 +81,7 @@ class ViewPort
             $bufferRows[$y] = ""
             #Fill row/string with char/cell data
             for ($x = 0; $x -lt $this.width; $x++) {
-                $bufferRows[$y] += $this.frameBuffer[$x,$y].char
+                $bufferRows[$y] += $this.frameBuffer[$x, $y].char
             }
         }
 
@@ -111,11 +91,15 @@ class ViewPort
         }
     }
 
-    [void] DrawParticle ([int]$xPos, [int]$yPos, [int]$depth ,[char]$char) {
+    [void] DrawParticle ([int]$xPos, [int]$yPos, [int]$depth , [char]$char) {
+        #Out of bounds check
+        if (($xPos -ge 0) -and ($xPos -lt $this.width)) {
+            if (($yPos -ge 0) -and ($yPos -lt $this.height)) {
+                $this.frameBuffer[$xPos, $yPos].depth = $depth
+                $this.frameBuffer[$xPos, $yPos].char = $char
+            }
+        }
         
-        $this.frameBuffer[$xPos,$yPos].depth = $depth
-        $this.frameBuffer[$xPos,$yPos].char = $char
-        Write-Host "Drawing Particle $($xPos), $($yPos), $($char)"
         
     }
     
@@ -124,11 +108,76 @@ class ViewPort
 
 
 
+class Player {
+    [int]$posX = 0
+    [int]$posY = 0
+
+    Player () {
+
+    }
+
+}
+
+
+class Input {
+    
+
+    [string] GetInputString () {
+        
+        #Command Prompt
+        $Global:Host.UI.Write(": ")
+
+        #The returned string
+        #Built in the for loop
+        [string]$fullCommand = ""
+        
+
+        while (1 -eq 1) {
+            [System.Management.Automation.Host.KeyInfo]$userInput = $Global:Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+
+            if ($userInput.VirtualKeyCode -eq "13") {
+                return $fullCommand
+                
+            } else {
+                $fullCommand += [string]$userInput.Character
+                $Global:Host.UI.Write($userInput.Character)
+            }
+        } 
+
+        return "Invalid Exit in GetInputString()"
+    }
+}
 
 
 
 
+class GameConsole {
+    [string[]]$consoleHistory
 
+    GameConsole ([int]$historyLength) {
+        $this.consoleHistory = New-Object 'string[]' $historyLength
+    }
+
+    [void] Log ([string]$entry) {
+        $shortenedLog = $this.consoleHistory[0..($this.consoleHistory.Length - 2)]
+        $this.consoleHistory = ,$entry + $shortenedLog
+    }
+
+    [void] DrawLog ($xPos, $yPos) {
+        #Move Cursor to Draw location
+        $oldPos = $Global:Host.UI.RawUI.CursorPosition
+        $Global:Host.UI.RawUI.CursorPosition = [System.Management.Automation.Host.Coordinates]::new($xPos,$yPos)
+        
+        #Draw Log
+        for ($i = 0; $i -lt $this.consoleHistory.Length; $i++) {
+            Write-Host $this.consoleHistory[$i]
+        }
+
+        #Move Cursor Back
+        $Global:Host.UI.RawUI.CursorPosition = $oldPos
+    }
+
+}
 
 
 
@@ -137,16 +186,13 @@ class ViewPort
 
 
 #Functions
-Function pause ($message)
-{
+Function pause ($message) {
     # Check if running Powershell ISE
-    if ($psISE)
-    {
+    if ($psISE) {
         Add-Type -AssemblyName System.Windows.Forms
         [System.Windows.Forms.MessageBox]::Show("$message")
     }
-    else
-    {
+    else {
         Write-Host "$message" -ForegroundColor Yellow
         $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
     }
@@ -160,59 +206,99 @@ Function pause ($message)
 
 ######################Initialise Variables
 
+#GameInfo Vars
+[int]$gameStep = 0
+[bool]$stopGame = $false
 
-#Viewport
-$viewPortHeight = 10
-$viewPortWidth = 10
-[BufferCell]$defaultBufferCell = New-Object 'BufferCell' 255,'.'
-[ViewPort]$viewPort = New-Object 'ViewPort' $viewPortWidth,$viewPortHeight,$defaultBufferCell
+#Player Vars
+[Player]$player = [player]::new()
 
+#Input Vars
+[Input]$gameInput = [Input]::new()
 
+#Viewport Vars
+[int]$viewPortHeight = 10
+[int]$viewPortWidth = 10
+[BufferCell]$defaultBufferCell = New-Object 'BufferCell' 255, '.'
+[ViewPort]$viewPort = New-Object 'ViewPort' $viewPortWidth, $viewPortHeight, $defaultBufferCell
+
+#Console Vars
+[GameConsole]$gameConsole = [GameConsole]::new(5)
 
 #Prepare
-Clear-Host
 $host.UI.RawUI.WindowTitle = 'Ardlinam'
-
-
-#Draw World
-
-
-
-#testing========================================
-
 pause('Start of Program, Press any key to continue...')
-#Write-Output $viewPort.frameBuffer[0,2].char
-<#$image = New-Object 'char[,]' 2,2
 
-$image[0,0] = 'a'
-$image[1,0] = 'b'
-$image[0,1] = 'c'
-$image[1,1] = 'd'
+#Game Loop
+while ($stopGame -eq $false) {
+    
+    #Prepare viewport
+    $viewPort.ClearFrameBuffer()
+    $viewPort.DrawParticle($player.posX, $player.posY, 10, 'O')
 
-$sprite = New-Object 'Sprite' 2,2,0,0,$image#>
+    #Draw viewport to screen
+    Clear-Host
+    $viewPort.DrawFrameBuffer()
 
-Write-Host $viewPort.frameBuffer[0,0].char
-$viewPort.DrawParticle(1,1,10,'a')
-Write-Host $viewPort.frameBuffer[0,0].char
+    #Draw Console
+    $gameConsole.DrawLog($host.UI.RawUI.CursorPosition.X, ($host.UI.RawUI.CursorPosition.Y + 1))
 
-$viewPort.DrawFrameBuffer()
-#Write-Output ("" + $port.frameBuffer[0,1].char)
+    
+
+    #Gather Player input
+    #Loop until valid input to avoid flicker
+    while ($true) {
+        $inputKey = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode
+
+        if ($inputKey -eq '40') {
+            #Down
+            $player.posY += 1
+            break
+        }
+        if ($inputKey -eq '39') {
+            #Right
+            $player.posX += 1
+            break
+        }
+    
+        if ($inputKey -eq '38') {
+            #Up
+            $player.posY -= 1
+            break
+        }
+    
+        if ($inputKey -eq '37') {
+            #Left
+            $player.posX -= 1
+            break
+        }
+    
+        if ($inputKey -eq '27') {
+            #Esc
+            $stopGame = $true
+            break
+        }
+    
+        if ($inputKey -eq '13') {
+            #Enter
+            [string]$consoleInput = $gameInput.GetInputString()
+            $gameConsole.Log($consoleInput)
+            
+            if ($consoleInput -eq "exit") {
+                $stopGame = $true
+            }
+
+            break
+        }
+    }
+    
 
 
 
-<#$Input = Read-Host -Prompt 'Please enter your name.'
-$PlayerName = $Input
-
-#Instructions
-Clear-Host
-Write-Output "Welcome to Ardlinam $PlayerName `n"
-Write-Output 'Please read the below instructions'
-Write-Output 'type "help" to list available commands'
-Write-Output "commands are not case sensitive `n"
-pause('Press any key to begin your adventure!')
+    $gameStep++
+    #pause('End of Step, Press any key to continue...')
+} 
 
 
 
-
-#>
 pause('End of Program, Press any key to continue...')
