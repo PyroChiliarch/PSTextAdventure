d5598ba17f62adaa83f9f0201795cc5fc3d76b3b
PyroChiliarch <pyrochiliarch@gmail.com>
2021-03-07 19:59:18 +1100

    Wiping

--- README.md
+++
@@ -1,2 +0,0 @@
-# PSTextAdventure
- Powershell Text Adventure
--- displayInput.ps1
+++
@@ -1,4 +0,0 @@
-
-DO {
-    Write-Host $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown').VirtualKeyCode #Write code to screen
-} while (1 -eq 1)
